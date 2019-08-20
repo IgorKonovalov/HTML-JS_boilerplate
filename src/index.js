bf16c9b07dbe31fa024404d8f774c5c16db615c2
Ilia Gusevskii <ilia_gusevskii@epam.com>
2019-08-20 09:37:38 +0300

    Add functionality to create new tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import './style.css';
 
-console.log({
-  a: '2',
-  b: '3',
-});
+const timePatternOptions = { hour: '2-digit', minute: '2-digit' };
+
+const newTaskInput = document.getElementById('add-task-input');
+const openTasks = document.getElementById('open-tasks');
+const doneTasks = document.getElementById('done-tasks');
+
+const addTaskButton = document.getElementById('add-task-button');
+addTaskButton.addEventListener('click', addNewTask);
+newTaskInput.addEventListener('keypress', addNewTask);
+
+function addNewTask(event) {
+  if (newTaskInput.value === '') {
+    return;
+  }
+
+  if (event.srcElement.id === 'add-task-button' || event.code === 'Enter') {
+    const newTaskDiv = document.createElement('div');
+
+    const taskCheckBox = document.createElement('input');
+    taskCheckBox.setAttribute('type', 'checkbox');
+
+    const taskTitle = document.createElement('div');
+    taskTitle.innerHTML = newTaskInput.value;
+    newTaskInput.value = '';
+
+    const tastCreationTime = document.createElement('div');
+    tastCreationTime.innerHTML = new Date().toLocaleTimeString("en-US", timePatternOptions);
+
+    newTaskDiv.appendChild(taskCheckBox);
+    newTaskDiv.appendChild(taskTitle);
+    newTaskDiv.appendChild(tastCreationTime);
+
+    openTasks.appendChild(newTaskDiv);
+  }
+}
